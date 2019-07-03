9b1bb94650f085226adfdac7027843bee8bc36fe
Knuppe <knpdesign@outlook.com>
2019-07-03 12:15:31 +0200

    Primer commit

---
+++ hbs/helpers.js
@@ -0,0 +1,20 @@
+const hbs = require('hbs')
+
+//Helpers
+hbs.registerHelper('getYear', () => {
+
+  return new Date().getFullYear()
+
+})
+
+hbs.registerHelper('captalizar', texto => {
+
+  let palabras = texto.split(' ')
+
+  palabras.forEach( (palabra, index) => {
+    palabras[index] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase()
+  })
+
+  return palabras.join(' ')
+
+})
